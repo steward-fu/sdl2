e7fe0c12b3efc79e6ef5d4ea2cc159ac43fc0108
steward-fu <steward.fu@gmail.com>
2023-12-03 10:16:29 -0500

    Used different launch.sh files

---
+++ assets/trimui/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mydir=`dirname "$0"`
+
+export HOME=$mydir
+export PATH=$mydir:$PATH
+export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
+export SDL_VIDEODRIVER=mmiyoo
+export SDL_AUDIODRIVER=mmiyoo
+export EGL_VIDEODRIVER=mmiyoo
+
+cd $mydir
+./test
