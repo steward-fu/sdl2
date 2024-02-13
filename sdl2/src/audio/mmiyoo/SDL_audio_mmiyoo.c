b5651ad4c66d7a36cbc9b1ca979b9973e98ff20d
steward-fu <steward.fu@gmail.com>
2024-02-13 11:20:59 -0500

    Set volume according to system setting

--- sdl2/src/audio/mmiyoo/SDL_audio_mmiyoo.c
+++ sdl2/src/audio/mmiyoo/SDL_audio_mmiyoo.c
@@ -35,6 +35,7 @@
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <linux/soundcard.h>
+#include <json-c/json.h>
 
 #include "SDL_timer.h"
 #include "SDL_audio.h"
@@ -48,16 +49,111 @@
 #include "mi_common_datatype.h"
 #include "mi_ao.h"
 
+#define MAX_VOLUME      20
+#define MIN_RAW_VALUE   -60
+#define MAX_RAW_VALUE   30
+#define MI_AO_SETVOLUME 0x4008690b
+#define MI_AO_GETVOLUME 0xc008690c
+#define MI_AO_SETMUTE   0x4008690d
+#define JSON_APP_FILE   "/appconfigs/system.json"
+#define JSON_VOL_KEY    "vol"
+
+static int cur_volume = 0;
 static MI_AUDIO_Attr_t stSetAttr;
 static MI_AUDIO_Attr_t stGetAttr;
 static MI_AO_CHN AoChn = 0;
 static MI_AUDIO_DEV AoDevId = 0;
+static struct json_object *jfile = NULL;
 #endif
 
 #ifdef TRIMUI
 static int dsp_fd = -1;
 #endif
 
+#ifdef MMIYOO
+static int set_volume_raw(int value, int add)
+{
+    int fd = -1;
+    int buf2[] = {0, 0}, prev_value = 0;
+    uint64_t buf1[] = {sizeof(buf2), (uintptr_t)buf2};
+
+    if ((fd = open("/dev/mi_ao", O_RDWR)) < 0) {
+        return 0;
+    }
+
+    ioctl(fd, MI_AO_GETVOLUME, buf1);
+    prev_value = buf2[1];
+
+    if (add) {
+        value = prev_value + add;
+    }
+    else {
+        value += MIN_RAW_VALUE;
+    }
+
+    if (value > MAX_RAW_VALUE) {
+        value = MAX_RAW_VALUE;
+    }
+    else if (value < MIN_RAW_VALUE) {
+        value = MIN_RAW_VALUE;
+    }
+
+    if (value == prev_value) {
+        close(fd);
+        return prev_value;
+    }
+
+    buf2[1] = value;
+    ioctl(fd, MI_AO_SETVOLUME, buf1);
+    if (prev_value <= MIN_RAW_VALUE && value > MIN_RAW_VALUE) {
+        buf2[1] = 0;
+        ioctl(fd, MI_AO_SETMUTE, buf1);
+    }
+    else if (prev_value > MIN_RAW_VALUE && value <= MIN_RAW_VALUE) {
+        buf2[1] = 1;
+        ioctl(fd, MI_AO_SETMUTE, buf1);
+    }
+    close(fd);
+    return value;
+}
+
+static int set_volume(int volume)
+{
+    int volume_raw = 0;
+
+    if (volume > MAX_VOLUME) {
+        volume = MAX_VOLUME;
+    }
+    else if (volume < 0) {
+        volume = 0;
+    }
+
+    if (volume != 0) {
+        volume_raw = round(48 * log10(1 + volume));
+    }
+
+    set_volume_raw(volume_raw, 0);
+    return volume;
+}
+
+int volume_inc(void)
+{
+    if (cur_volume < MAX_VOLUME) {
+        cur_volume+= 1;
+        set_volume(cur_volume);
+    }
+    return cur_volume;
+}
+
+int volume_dec(void)
+{
+    if (cur_volume > 0) {
+        cur_volume-= 1;
+        set_volume(cur_volume);
+    }
+    return cur_volume;
+}
+#endif
 static void MMIYOO_CloseDevice(_THIS)
 {
     SDL_free(this->hidden->mixbuf);
@@ -102,6 +198,19 @@ static int MMIYOO_OpenDevice(_THIS, void *handle, const char *devname, int iscap
     }
 
 #ifdef MMIYOO
+    jfile = json_object_from_file(JSON_APP_FILE);
+    if (jfile != NULL) {
+        struct json_object *volume = NULL;
+
+        json_object_object_get_ex(jfile, JSON_VOL_KEY, &volume);
+        cur_volume = json_object_get_int(volume);
+        //json_object_object_add(jfile, JSON_VOL_KEY, json_object_new_int(2));
+        //json_object_to_file(JSON_APP_FILE, jfile);
+        json_object_put(jfile);
+    }
+#endif
+
+#ifdef MMIYOO
     stSetAttr.eBitwidth = E_MI_AUDIO_BIT_WIDTH_16;
     stSetAttr.eWorkmode = E_MI_AUDIO_MODE_I2S_MASTER;
     stSetAttr.u32FrmNum = 6;
@@ -141,6 +250,7 @@ static int MMIYOO_OpenDevice(_THIS, void *handle, const char *devname, int iscap
     stAoChn0OutputPort0.u32ChnId = AoChn;
     stAoChn0OutputPort0.u32PortId = 0;
     MI_SYS_SetChnOutputPortDepth(&stAoChn0OutputPort0, 12, 13);
+    set_volume(cur_volume);
 #endif
 
 #ifdef TRIMUI
@@ -158,6 +268,7 @@ static int MMIYOO_OpenDevice(_THIS, void *handle, const char *devname, int iscap
     arg = FREQ;
     ioctl(dsp_fd, SOUND_PCM_WRITE_RATE, &arg);
 #endif
+
     return 0;
 }
 
