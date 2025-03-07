aad5aeef139b268d4ffbc064bd0f30533c7da32b
steward-fu <steward.fu@gmail.com>
2025-03-07 13:53:17 +0800

    Refined.

--- Makefile.mk
+++ Makefile.mk
@@ -22,16 +22,7 @@ REL_VER  = $(shell git rev-parse HEAD | cut -c 1-8)
 sdl2:
 	make -C sdl2 -j4
 
-.PHONY: example
-example:
-	cp assets/$(MOD)/* example/
-	cp sdl2/build/.libs/libSDL2-2.0.so.0 example/lib/
-	$(CC) example/sdl2.c $(SDL2_INC) -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_gfx -lSDL2_ttf -lGLESv2 -o example/sdl2
-	$(CXX) example/opengles.cpp -Wno-narrowing $(SDL2_INC) -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_gfx -lSDL2_ttf -lGLESv2 -o example/opengles
-
 .PHONY: clean
 clean:
-	cd example && rm -rf sdl2 opengles launch.sh config.json
-	cd example/lib && rm -rf libMali.so libEGL.so libGLESv2.so libSDL2-2.0.so.0
 	cd sdl2 && rm -rf libMali.so libEGL.so libGLESv2.so
 	make -C sdl2 distclean
